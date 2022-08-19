bde64ad27fc7c89c616055bc6315034023af7066
ImNeroo <e.z.millan07@gmail.com>
2022-08-19 16:38:22 -0500

    finished project

--- main.py
+++ main.py
@@ -2,56 +2,68 @@ import tkinter
 import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
-score = 0
-# ---------FUNCTIONS-------------
-def right_answer():
-    global score
-    language_word = "1"
-    word_to_show = "2"
-    canvas.create_image(400, 262, image=back_card)
-    canvas.grid(column=0, row=0, columnspan=2)
-    canvas.create_text(400, 150, text=language_word)
-    canvas.create_text(400, 265, text=word_to_show)
-    score += 1
-def wrong_answer():
-    language_word = "1"
-    word_to_show = "2"
-    canvas.create_image(400, 262, image=back_card)
-    canvas.grid(column=0, row=0, columnspan=2)
-    canvas.create_text(400, 150, text=language_word)
-    canvas.create_text(400, 265, text=word_to_show)
-
+current_card = {}
 # ----------READING DATA ---------
-data = pandas.read_csv("./data/french_words.csv")
-new_dictionary = pandas.DataFrame.todict(orient="records")
+try:
+    data = pandas.read_csv("./data/words_to_learn.csv")
 
-word_to_show = ""
-language_word = ""
+except FileNotFoundError:
+    data = pandas.read_csv("./data/french_words.csv")
+
+new_dictionary = data.to_dict(orient="records")
+
+
+
+# ---------FUNCTIONS-------------
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(new_dictionary)
+    canvas.itemconfig(card_image, image=front_card)
+    canvas.itemconfig(title, text="French", fill="black")
+    canvas.itemconfig(word_text, text=current_card["French"], fill="black")
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_image, image=back_card)
+    canvas.itemconfig(title, text="English", fill="white")
+    canvas.itemconfig(word_text, text=current_card["English"], fill="white")
+
+def right_answer():
+    new_dictionary.remove(current_card)
+    print(len(new_dictionary))
+    data = pandas.DataFrame(new_dictionary)
+    data.to_csv("./data/words_to_learn.csv", index=False)
+    next_card()
+
+
 # ---------UI-------------------
 # ------WINDOW
 window = tkinter.Tk()
 window.title("Flash card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
+flip_timer = window.after(3000, flip_card)
 # ----CARDS
 front_card = tkinter.PhotoImage(file="./images/card_front.png")
 back_card = tkinter.PhotoImage(file="./images/card_back.png")
 
 # -----CANVAS
 canvas = tkinter.Canvas(width=800, height=525, highlightthickness=0, bg=BACKGROUND_COLOR)
-canvas.create_image(400, 262, image=front_card)
+card_image = canvas.create_image(400, 263, image=front_card)
 canvas.grid(column=0, row=0, columnspan=2)
-canvas.create_text(400, 150, text=language_word, font=("Arial", 40, "italic"))
-canvas.create_text(400, 265, text=word_to_show, font=("Arial", 60, "bold"))
+title = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 
-
-
-
-#----BUTTONS
+# ----BUTTONS
 right_image = tkinter.PhotoImage(file="./images/right.png")
 wrong_image = tkinter.PhotoImage(file="./images/wrong.png")
 right_button = tkinter.Button(image=right_image, highlightthickness=0, command=right_answer)
 right_button.grid(column=1, row=1)
-wrong_button = tkinter.Button(image=wrong_image, highlightthickness=0, command=wrong_answer)
+wrong_button = tkinter.Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
+
+next_card()
+
 window.mainloop()
+
